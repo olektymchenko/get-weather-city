11020e7fb26d36a7005325c41af6402905671409
olektymchenko <58951697+olektymchenko@users.noreply.github.com>
2020-03-30 21:47:27 +0200

    Updates

---
+++ precache-manifest.cd5f660d4992c0655c9aadd5ed0c386c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57250d41a5db5768193089e8cdd0aec",
+    "url": "/get-weather-city/index.html"
+  },
+  {
+    "revision": "3bb7a32e2731eaccb2cc",
+    "url": "/get-weather-city/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bc28d1fd31652d84ea00",
+    "url": "/get-weather-city/static/css/main.251431d2.chunk.css"
+  },
+  {
+    "revision": "3bb7a32e2731eaccb2cc",
+    "url": "/get-weather-city/static/js/2.b2107b58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/get-weather-city/static/js/2.b2107b58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc28d1fd31652d84ea00",
+    "url": "/get-weather-city/static/js/main.7cd4ea5b.chunk.js"
+  },
+  {
+    "revision": "51a21c723e6ece9f3768",
+    "url": "/get-weather-city/static/js/runtime-main.52931ea1.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/get-weather-city/static/media/polygon.cc54c19b.png"
+  }
+]);
